d2354ee83552bdbfd2701ff13ae0a337541797e2
System <112185174+pw56@users.noreply.github.com>
2025-04-20 20:01:15 +0900

    あ

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Button.h"
+#include "NumberLed.h"
+
+const int btn1 = ;
+const int btn2 = ;
+const int btn3 = ;
+const int btn4 = ;
+const int btn5 = ;
+const int btn6 = ;
+
+void setup() {
+  Button.begin(btn1, btn2, btn3, btn4, btn5, btn6);
+  NumLed.begin();
+}
+
+void loop() {
+  ;
+}
